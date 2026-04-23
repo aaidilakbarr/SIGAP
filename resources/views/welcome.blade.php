e7a384a58fe3ebdaec3a037b9a3672652397069a
Aidil <aidilakbar05@gmail.com>
2026-04-23 09:54:49 +0700

    Fixing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIGAP</title>
-    <link rel="icon" type="image/svg+xml" href="{{ Vite::asset('resources\js\assets\icon_web.png') }}" />
+    <link rel="icon" type="image/png" href="{{asset('icon_web.png') }}" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet">
     <script>window.APP_URL = "{{ url('/') }}";</script>
     @viteReactRefresh
